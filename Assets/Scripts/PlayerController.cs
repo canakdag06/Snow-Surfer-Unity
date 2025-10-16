65b6163d7ef39806c3d0f3aba588ba4deb49f98c
Can Akdag <canakdag06@gmail.com>
2025-10-16 19:26:09 +0300

    created PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    InputAction moveAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+    }
+}
