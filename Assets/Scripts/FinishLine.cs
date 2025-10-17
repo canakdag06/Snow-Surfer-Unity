81a529cd3e257e787ea57ef33652d6c55638a892
Can Akdag <canakdag06@gmail.com>
2025-10-17 19:10:58 +0300

    created FinishLine.cs

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    private int playerLayer;
+
+    private void Start()
+    {
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == playerLayer)
+        {
+            Debug.Log("LEVEL COMPLETED!");
+        }
+    }
+}
