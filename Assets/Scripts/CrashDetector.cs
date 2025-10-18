fa28c09baab78ac9a7a5cd32f13f16fae760cfae
Can Akdag <canakdag06@gmail.com>
2025-10-18 21:10:41 +0300

    isCrashed check

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,15 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashParticle;
     [SerializeField] int reloadDelay;
+
+    private bool isCrashed;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
 
-        if (collision.gameObject.layer == layerIndex)
+        if (collision.gameObject.layer == layerIndex && !isCrashed)
         {
+            isCrashed = true;
             Debug.Log("LEVEL FAILED...");
+            crashParticle.Play();
             Invoke("LoadLevel", reloadDelay);
         }
     }
