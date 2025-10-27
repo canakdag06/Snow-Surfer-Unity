1ec5f3ec2edff94f1c8856833940f222f9053f12
Can Akdag <canakdag06@gmail.com>
2025-10-27 16:35:13 +0300

    updated SnowTrail.cs

--- Assets/Scripts/SnowTrail.cs
+++ Assets/Scripts/SnowTrail.cs
@@ -6,11 +6,21 @@ public class SnowTrail : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        snowParticles.Play();
+        int layerIndex = LayerMask.NameToLayer("Floor");
+
+        if (collision.gameObject.layer == layerIndex)
+        {
+            snowParticles.Play();
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        snowParticles.Stop();
+        int layerIndex = LayerMask.NameToLayer("Floor");
+
+        if (collision.gameObject.layer == layerIndex)
+        {
+            snowParticles.Stop();
+        }
     }
 }
